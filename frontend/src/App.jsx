0d7fa79e56ccb2a40c018277b81c8941568031f2
Mhmd Fazil <fk2227487@gmail.com>
2023-12-16 15:29:14 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+// Importing necessary modules and components
 import './App.css'
 import {
   BrowserRouter as Router,
@@ -9,15 +10,20 @@ import CategoryComponentWithGST from './CategoryComponentWithGST'
 import ProductComponent from './ProductComponent'
 import ProductTableComponent from './ProductTableComponent'
 
-
+// Main App component
 function App() {
   return (
     <>
+      {/* Setting up the router for navigation */}
       <Router>
         <Routes>
+          {/* Route for the default category component */}
           <Route path="/" Component={CategoryComponent} />
+          {/* Route for the category component with GST */}
           <Route path="/gst" Component={CategoryComponentWithGST} />
+          {/* Route for the product component */}
           <Route path="/product" Component={ProductComponent} />
+          {/* Route for the product table component */}
           <Route path="/sale" Component={ProductTableComponent} />
         </Routes>
       </Router>
@@ -25,4 +31,5 @@ function App() {
   )
 }
 
+// Exporting the App component as the default export
 export default App
